ada54ef6c14bd6658587df92834ec9fa6268ab5f
Soraya Guariente <Soraya.biomed@gmail.com>
2021-12-06 17:59:06 -0300

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: "Mon, 12:06",
+      description: response.data.weather[0].description,
+      icon: "https://trusting-brown-57e448.netlify.app/images/04d.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      country: response.data.sys.country,
+    });
+  }
+  if (weatherData.ready) {
+    return (<div className="Weather">
       <div className="container">
         <div className="row">
           <div className="col-7">
@@ -31,28 +46,41 @@ export default function Weather() {
         </div>
       </div>
 
-      <div className="cityName">London</div>
+      <div className="cityName">
+        {weatherData.city}, {weatherData.country}
+      </div>
       <div className="cityWeatherData container">
         <div className="row">
           <div className="col-4">
-            <div className="currentTime">Mon, 12:06</div>
+            <div className="currentTime">{weatherData.date}</div>
             <br />
-            <div className="currentDescription">Broken clouds</div>
-            <div className="currentHumidity">Humidity: 92%</div>
-            <div className="currentWind">Wind: 0km/h</div>
+            <div className="currentDescription">{weatherData.description}</div>
+            <div className="currentHumidity">
+              Humidity: {Math.round(weatherData.humidity)}%
+            </div>
+            <div className="currentWind">Wind: {Math.round(weatherData.wind)}km/h</div>
           </div>
           <div className="col-2">
-            <img
-              src="https://trusting-brown-57e448.netlify.app/images/04d.png"
-              alt="Broken clouds"
-            />
+            <img src={weatherData.icon} alt={weatherData.description} />
           </div>
           <div className="col-5">
-            <span className="currentTemperature">7 </span>{" "}
+            <span className="currentTemperature">
+              {Math.round(weatherData.temperature)}{" "}
+            </span>{" "}
             <span classname="celsiusUnit">°C</span>
           </div>
         </div>
       </div>
-    </div>
+    </div>);
+  }
+  else {
+
+  const apiKey = "f1eea97ae866b4f1ba1d0c6161e558e3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    "Loading..."
+    
   );
+  }
 }

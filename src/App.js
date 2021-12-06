f26965229271680dd5294259cd3d50b5d761d81c
Soraya Guariente <Soraya.biomed@gmail.com>
2021-12-06 11:11:08 -0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-          </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/soraya-guariente-04b1a3166/"
+          target="_blank"
+        >
+          {" "}
+          Soraya Guariente{" "}
+        </a>{" "}
+        and is open-sorced on{" "}
+        <a
+          href="https://github.com/sorayaguariente/react-weather-dark-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
-
-

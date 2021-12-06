9856abdb4714811f6b73f70e3cef4f7ce1715afe
Soraya Guariente <Soraya.biomed@gmail.com>
2021-12-06 19:23:39 -0300

    Added date on the weather component

---
+++ src/FormattedDate.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function FormattedDate(props) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -10,7 +11,7 @@ export default function Weather(props) {
       coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Mon, 12:06",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       icon: "https://trusting-brown-57e448.netlify.app/images/04d.png",
       wind: response.data.wind.speed,
@@ -19,68 +20,71 @@ export default function Weather(props) {
     });
   }
   if (weatherData.ready) {
-    return (<div className="Weather">
-      <div className="container">
-        <div className="row">
-          <div className="col-7">
-            <form>
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="citySearch"
-                autoFocus="on"
-              />
-              <input
-                type="submit"
-                value="Search"
-                className="search btn-primary"
-              />
-            </form>
-          </div>
-          <div className="col-5">
-            <span className="locationLink">
-              {" "}
-              Or use <a href="/">your current location </a>{" "}
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="cityName">
-        {weatherData.city}, {weatherData.country}
-      </div>
-      <div className="cityWeatherData container">
-        <div className="row">
-          <div className="col-4">
-            <div className="currentTime">{weatherData.date}</div>
-            <br />
-            <div className="currentDescription">{weatherData.description}</div>
-            <div className="currentHumidity">
-              Humidity: {Math.round(weatherData.humidity)}%
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="row">
+            <div className="col-7">
+              <form>
+                <input
+                  type="search"
+                  placeholder="Enter a city"
+                  className="citySearch"
+                  autoFocus="on"
+                />
+                <input
+                  type="submit"
+                  value="Search"
+                  className="search btn-primary"
+                />
+              </form>
             </div>
-            <div className="currentWind">Wind: {Math.round(weatherData.wind)}km/h</div>
+            <div className="col-5">
+              <span className="locationLink">
+                {" "}
+                Or use <a href="/">your current location </a>{" "}
+              </span>
+            </div>
           </div>
-          <div className="col-2">
-            <img src={weatherData.icon} alt={weatherData.description} />
-          </div>
-          <div className="col-5">
-            <span className="currentTemperature">
-              {Math.round(weatherData.temperature)}{" "}
-            </span>{" "}
-            <span classname="celsiusUnit">°C</span>
+        </div>
+
+        <div className="cityName">
+          {weatherData.city}, {weatherData.country}
+        </div>
+        <div className="cityWeatherData container">
+          <div className="row">
+            <div className="col-4">
+              <div className="currentTime">
+                <FormattedDate date={weatherData.date} />
+              </div>
+              <br />
+              <div className="currentDescription">
+                {weatherData.description}
+              </div>
+              <div className="currentHumidity">
+                Humidity: {Math.round(weatherData.humidity)}%
+              </div>
+              <div className="currentWind">
+                Wind: {Math.round(weatherData.wind)}km/h
+              </div>
+            </div>
+            <div className="col-2">
+              <img src={weatherData.icon} alt={weatherData.description} />
+            </div>
+            <div className="col-5">
+              <span className="currentTemperature">
+                {Math.round(weatherData.temperature)}{" "}
+              </span>{" "}
+              <span classname="celsiusUnit">°C</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>);
-  }
-  else {
-
-  const apiKey = "f1eea97ae866b4f1ba1d0c6161e558e3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    "Loading..."
-    
-  );
+    );
+  } else {
+    const apiKey = "f1eea97ae866b4f1ba1d0c6161e558e3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
   }
 }

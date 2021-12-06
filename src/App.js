237f6f570674b5887c3d2371f02d0e0968e5ea7b
Soraya Guariente <Soraya.biomed@gmail.com>
2021-12-06 11:44:04 -0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/soraya-guariente-04b1a3166/"
-          target="_blank"
-        >
-          {" "}
-          Soraya Guariente{" "}
-        </a>{" "}
-        and is open-sorced on{" "}
-        <a
-          href="https://github.com/sorayaguariente/react-weather-dark-app"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/soraya-guariente-04b1a3166/"
+            target="_blank"
+          >
+            {" "}
+            Soraya Guariente{" "}
+          </a>{" "}
+          and is open-sorced on{" "}
+          <a
+            href="https://github.com/sorayaguariente/react-weather-dark-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

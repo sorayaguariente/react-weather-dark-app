d4c87f78f85433b6c2c86e5d7e24ecec77600e6a
Soraya Guariente <Soraya.biomed@gmail.com>
2021-12-10 11:01:34 -0300

    Added useEffect and fixed some css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -22,6 +25,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,5 +1,6 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecastDay.css";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
@@ -17,16 +18,15 @@ export default function WeatherForecastDay(props) {
     return days[day];
   }
   return (
-    <div className="WeatherForecastDay">
-      <div className="WeatherForecast-day">{date()}</div>
-      <div className="row">
-        <div className="col-5">
-          <WeatherIcon code={props.data.weather[0].icon} size={32} />
-        </div>
-        <div className="col-6">
-          <div className="WeatherForecast-max">{maxTemperature()}</div>
-          <div className="WeatherForecast-min">{minTemperature()}</div>
-        </div>
+    <div className="WeatherForecastDay text-center">
+      <div className="WeatherForecast-day"> {date()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={32} />
+      <div className="WeatherForecastDay-temps">
+        <span className="WeatherForecast-max fw-bold">{maxTemperature()}°</span>
+        {"  "}
+        <span className="WeatherForecast-min fw-light">
+          {minTemperature()}°
+        </span>
       </div>
     </div>
   );
